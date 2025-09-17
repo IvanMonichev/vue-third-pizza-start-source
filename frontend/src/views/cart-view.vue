3ef3e64d79fe18a4ea99fa4c118d392a551456cf
IvanMonichev <risen15@yandex.ru>
2025-09-17 23:08:43 +0300

    feat: add cart footer

--- frontend/src/views/cart-view.vue
+++ frontend/src/views/cart-view.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import AppCounter from '@/common/components/app-counter.vue'
 import AppTitle from '@/common/components/app-title.vue'
+import CartFooter from '@/modules/cart/cart-footer.vue'
 </script>
 
 <template>
@@ -194,23 +195,7 @@ import AppTitle from '@/common/components/app-title.vue'
         </div>
       </div>
     </main>
-    <section class="footer">
-      <div class="footer__more">
-        <a href="#" class="button button--border button--arrow"
-          >Хочу еще одну</a
-        >
-      </div>
-      <p class="footer__text">
-        Перейти к конструктору<br />чтоб собрать ещё одну пиццу
-      </p>
-      <div class="footer__price">
-        <b>Итого: 2 228 ₽</b>
-      </div>
-
-      <div class="footer__submit">
-        <button type="submit" class="button">Оформить заказ</button>
-      </div>
-    </section>
+    <CartFooter />
   </form>
 </template>
 
@@ -402,44 +387,5 @@ import AppTitle from '@/common/components/app-title.vue'
   border: 0;
 
   clip-path: inset(100%);
-}
-
-.footer {
-  display: flex;
-  align-items: center;
-
-  margin-top: auto;
-  padding: 25px 2.12%;
-
-  background-color: rgba(ds-colors.$green-500, 0.1);
-}
-
-.footer__more {
-  width: 220px;
-  margin-right: 16px;
-
-  a {
-    padding-top: 16px;
-    padding-bottom: 16px;
-  }
-}
-
-.footer__text {
-  @include ds-typography.l-s11-h13;
-
-  color: rgba(ds-colors.$black, 0.5);
-}
-
-.footer__price {
-  @include ds-typography.b-s24-h28;
-
-  margin-right: 12px;
-  margin-left: auto;
-}
-
-.footer__submit {
-  button {
-    padding: 16px 14px;
-  }
 }
 </style>
