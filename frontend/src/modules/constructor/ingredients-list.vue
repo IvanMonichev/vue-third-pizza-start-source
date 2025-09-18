b79cf763f9448bf7b69bb8baf060e30aacff02cf
IvanMonichev <risen15@yandex.ru>
2025-09-18 11:13:21 +0300

    refactor: return styles for ingredients

--- frontend/src/modules/constructor/ingredients-list.vue
+++ frontend/src/modules/constructor/ingredients-list.vue
@@ -28,6 +28,9 @@ const viewIngredients = props.ingredients.map(mapIngredient)
 
 <style scoped lang="scss">
 @use '@/assets/sass/mixins/m_clear-list';
+@use '@/assets/sass/ds-system/ds-colors';
+@use '@/assets/sass/ds-system/ds-typography';
+@use '@/assets/sass/mixins/m_center';
 
 .ingredients__list {
   @include m_clear-list.clear-list;
@@ -48,5 +51,92 @@ const viewIngredients = props.ingredients.map(mapIngredient)
   width: 54px;
   margin-top: 10px;
   margin-left: 36px;
+}
+
+.filling {
+  @include ds-typography.r-s14-h16;
+
+  position: relative;
+
+  display: block;
+
+  padding-left: 36px;
+
+  &::before {
+    @include m_center.p_center-v;
+
+    display: block;
+
+    width: 32px;
+    height: 32px;
+
+    content: '';
+
+    border-radius: 50%;
+    background-color: ds-colors.$white;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 80% 80%;
+  }
+
+  &--tomatoes::before {
+    background-image: url('@/assets/img/filling/tomatoes.svg');
+  }
+
+  &--ananas::before {
+    background-image: url('@/assets/img/filling/ananas.svg');
+  }
+
+  &--bacon::before {
+    background-image: url('@/assets/img/filling/bacon.svg');
+  }
+
+  &--blue_cheese::before {
+    background-image: url('@/assets/img/filling/blue_cheese.svg');
+  }
+
+  &--cheddar::before {
+    background-image: url('@/assets/img/filling/cheddar.svg');
+  }
+
+  &--chile::before {
+    background-image: url('@/assets/img/filling/chile.svg');
+  }
+
+  &--ham::before {
+    background-image: url('@/assets/img/filling/ham.svg');
+  }
+
+  &--jalapeno::before {
+    background-image: url('@/assets/img/filling/jalapeno.svg');
+  }
+
+  &--mozzarella::before {
+    background-image: url('@/assets/img/filling/mozzarella.svg');
+  }
+
+  &--mushrooms::before {
+    background-image: url('@/assets/img/filling/mushrooms.svg');
+  }
+
+  &--olives::before {
+    background-image: url('@/assets/img/filling/olives.svg');
+  }
+
+  &--onion::before {
+    background-image: url('@/assets/img/filling/onion.svg');
+  }
+
+  &--parmesan::before {
+    background-image: url('@/assets/img/filling/parmesan.svg');
+  }
+
+  &--salami::before {
+    background-image: url('@/assets/img/filling/salami.svg');
+  }
+
+  &--salmon::before {
+    background-image: url('@/assets/img/filling/salmon.svg');
+  }
 }
 </style>
