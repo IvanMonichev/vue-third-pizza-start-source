94e301fbf76374435fac5e494a3c1f4d5645154c
IvanMonichev <risen15@yandex.ru>
2025-10-03 21:03:00 +0300

    refactor: rename options

--- frontend/src/common/constants/address-form.constants.ts
+++ frontend/src/common/constants/address-form.constants.ts
@@ -1,11 +1,11 @@
 import { DeliveryType } from '@/common/enums/delivery-type.enum'
 import { Option } from '@/common/types/core.types'
 
-export const develiryTypeOptions: Array<Option<DeliveryType>> = [
+export const deliveryTypeOptions: Array<Option<DeliveryType>> = [
   {
     label: 'Заберу сам',
     value: DeliveryType.PICK_UP
   },
   { value: DeliveryType.NEW_ADDRESS, label: 'Новый адрес' },
   { value: DeliveryType.SAVED_ADDRESS, label: 'Дом' }
-] as const
+]
