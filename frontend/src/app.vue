382f351891db7491eda40f450d7273ba050fc3f0
IvanMonichev <risen15@yandex.ru>
2025-10-06 18:58:04 +0300

    feat: update header

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,8 +1,28 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/app-layout.vue'
+import { useAuthStore } from '@/store/auth.store'
+import { watch } from 'vue'
+import { useAuthUser } from './api/auth.api'
+import { tokenManager } from './services/token-manager'
+
+const { data: user, isLoading, isError } = useAuthUser()
+
+const authStore = useAuthStore()
+
+watch(user, (newUser) => {
+  if (newUser) {
+    authStore.setAuth({
+      user: newUser,
+      token: tokenManager.get()!
+    })
+  } else if (!isLoading.value && isError.value) {
+    authStore.clearAuth()
+  }
+})
+</script>
+
 <template>
   <AppLayout>
     <router-view></router-view>
   </AppLayout>
 </template>
-<script setup lang="ts">
-import AppLayout from '@/layouts/app-layout.vue'
-</script>
